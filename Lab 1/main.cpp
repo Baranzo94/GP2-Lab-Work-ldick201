07d35116835290dff9bdfac84aa04c36622312f6
Liam <liam.baranzo@gmail.com>
2014-09-23 10:45:36 +0100

    Saved and closed ddduuuuuhhhhhhhh

---
+++ Lab 1/main.cpp
@@ -0,0 +1,21 @@
+//Header files
+#include <iostream>
+
+//Global variables go here
+
+// Global functions
+void lnitWindow(int width, int height, bool fullscreen)
+{
+
+}
+
+void CleanUp()
+{
+
+}
+
+//Main Method - Entry Point
+int main(int argc, char*arg[])
+{
+	return 0;
+}
