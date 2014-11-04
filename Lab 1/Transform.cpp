a96b499d4ac26e14e6e9ceb0074a63caa3f16c92
Liam <liam.baranzo@gmail.com>
2014-11-04 10:37:04 +0000

    Lab 6 Task 3 Completed
    Moving onto Task 4 Mesh

---
+++ Lab 1/Transform.cpp
@@ -0,0 +1,56 @@
+#include "Transform.h"
+
+Transform::Transform()
+{
+	m_Position = vec3(0.0f, 0.0f, 0.0f);
+	m_Rotation = vec3(0.0f, 0.0f, 0.0f);
+	m_Scale = vec3(1.0f, 1.0f, 1.0f);
+	m_model = mat4();
+}
+
+Transform::Transform()
+{
+
+}
+
+void Transform::update()
+{
+	mat4 translate = glm::translate(translate, m_Position);
+	mat4 scale = glm::scale(scale, m_Scale);
+
+	mat4 rotationX = glm::rotate(rotationX, m_Rotation.x, vec3(1.0f, 0.0f, 0.0f));
+	mat4 rotationY = glm::rotate(rotationY, m_Rotation.y, vec3(0.0f, 1.0f, 0.0f));
+	mat4 rotationZ = glm::rotate(rotationZ, m_Rotation.z, vec3(0.0f, 0.0f, 1.0f));
+	mat4 rotation = rotationX*rotationY*rotationZ;
+
+	m_model = translate*rotation*scale;
+}
+
+void Transform::setPosition(float x, float y, float z)
+{
+	m_Position = vec3(x, y, z);
+}
+void Transform::setRotation(float x, float y, float z)
+{
+	m_Rotation = vec3(x, y, z);
+}
+void Transform::setScale(float x, float y, float z)
+{
+	m_Scale = vec3(x, y, z);
+}
+vec3& Transform::getPosition()
+{
+	return m_Position;
+}
+vec3& Transform::getScale()
+{
+	return m_Scale;
+}
+vec3& Transform::getRotation()
+{
+	return m_Rotation;
+}
+mat4& Transform::getModel()
+{
+	return m_model;
+}
---
+++ Lab 1/Transform.h
@@ -0,0 +1,49 @@
+#ifndef TRANSFORM_H
+
+#define TRANSFORM_H
+
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <string>
+
+#include <glm/glm.hpp>
+using glm::mat4;
+using glm::vec3;
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Transform
+{
+public:
+	Transform();
+	~Transform();
+
+	void update();
+
+	void setPosition(float x, float y, float z);
+	void setRotation(float x, float y, float z);
+	void setScale(float x, float y, float z);
+
+	vec3& getPosition();
+	vec3& getRotation();
+	vec3& getScale();
+	
+	mat4& getModel();
+
+protected:
+
+private:
+
+	vec3 m_Position;
+	vec3 m_Scale;
+	vec3 m_Rotation;
+	mat4 m_model;
+
+};
+
+
+
+#endif
