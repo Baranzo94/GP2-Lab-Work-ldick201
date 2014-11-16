d1c8ac0b447b87b54dc382a46fe7d6cf4326beb2
Liam <liam.baranzo@gmail.com>
2014-11-16 16:28:53 +0000

    Lab 6 Task 4 Mesh Completed
    Moving onto task 5 (Materials)

---
+++ Lab 1/Mesh.h
@@ -0,0 +1,41 @@
+#ifndef MESH_H
+
+#define MESH_H
+
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <string>
+
+class Mesh
+{
+public:
+
+	Mesh();
+	~Mesh();
+
+	void init();
+	void destroy();
+	void bind();
+
+	void copyVertexData(int count, int stride, void ** data);
+	void copyIndexData(int count, int stride, void ** data);
+
+	int getVertexCount();
+	int getIndexCount();
+	
+protected:
+	
+private:
+	GLuint m_VBO;
+	GLuint m_EBO;
+	GLuint m_VAO;
+
+	int m_VertexCount;
+	int m_IndexCount;
+};
+
+
+
+#endif
